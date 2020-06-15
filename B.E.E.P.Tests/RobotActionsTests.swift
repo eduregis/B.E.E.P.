5ca2f9f09f41191560da5264e6f225fdbaf20f0a
Nathália Moura <crds.nathalia@gmail.com>
2020-06-15 20:33:44 -0300

    Preparando as classes de teste

---
+++ B.E.E.P.Tests/RobotActionsTests.swift
@@ -0,0 +1,15 @@
+//
+//  RobotActionsTests.swift
+//  B.E.E.P.Tests
+//
+//  Created by Nathália Cardoso on 15/06/20.
+//  Copyright © 2020 Eduardo Oliveira. All rights reserved.
+//
+
+import XCTest
+
+@testable import B_E_E_P_
+
+class RobotActionsTests: XCTestCase {
+
+}
