46680b174321c8f0d99ec20e6ab153cccb16b2da
Nathália Moura <crds.nathalia@gmail.com>
2020-06-15 21:56:48 -0300

    Testes das ações do robo

--- B.E.E.P.Tests/RobotActionsTests.swift
+++ B.E.E.P.Tests/RobotActionsTests.swift
@@ -11,5 +11,50 @@ import XCTest
 @testable import B_E_E_P_
 
 class RobotActionsTests: XCTestCase {
-
+    
+    func test_conditional_objectAhead_containsObject() {
+        //given
+        let sut = GameScene()
+        let input1 = "infected"
+        
+        //when
+        let output1 = sut.objectAhead(type: input1)
+        
+        //then
+        XCTAssertEqual(output1, false)
+    }
+    
+    func test_conditional_cliffAhead_containsCliff() {
+        //given
+        let sut = GameScene()
+        
+        //when
+        let output1 = sut.cliffAhead()
+        
+        //then
+        XCTAssertEqual(output1, false)
+    }
+    
+    func test_action_moveRobot_isPossible() {
+        //given
+        let sut = GameScene()
+        
+        //when
+        let output1 = sut.moveRobot()
+        
+        //then
+        XCTAssertEqual(output1, true)
+    }
+    
+    func test_action_turnRobot_actionTurn() {
+        //given
+        let sut = GameScene()
+        let input1 = "left"
+        
+        //when
+        let output1 = sut.turnRobot(direction: input1)
+        
+        //then
+        XCTAssertNoThrow(output1)
+    }
 }
