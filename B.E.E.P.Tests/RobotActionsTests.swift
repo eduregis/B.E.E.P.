82404c76b1b3492f6a663b84cd329dd7e5c8e91b
Nathália Moura <crds.nathalia@gmail.com>
2020-06-17 17:14:47 -0300

    Atualizando os testes que cobriam funções que receberam novos parametros

--- B.E.E.P.Tests/RobotActionsTests.swift
+++ B.E.E.P.Tests/RobotActionsTests.swift
@@ -63,7 +63,7 @@ class RobotActionsTests: XCTestCase {
         let sut = RobotMoveComponent()
         let input1 = "left"
         //when
-        let output1 = sut.move(direction: input1)
+        let output1 = sut.move(direction: input1, box: false)
         
         //then
         XCTAssertNoThrow(output1)
@@ -75,7 +75,7 @@ class RobotActionsTests: XCTestCase {
         let input1 = "left"
         
         //when
-        let output1 = sut.turn(direction: input1)
+        let output1 = sut.turn(direction: input1, box: false)
         
         //then
         XCTAssertNoThrow(output1)
