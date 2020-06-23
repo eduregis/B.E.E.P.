5c23d38ad664a83f9a2136d8bc92f889531eec3e
Eduardo Regis <eduregis1@gmail.com>
2020-06-23 10:42:04 -0300

    changing entityManager approach to skspritenode in dialogues

--- B.E.E.P./Scenes/Extensions/DialoguesExtension.swift
+++ B.E.E.P./Scenes/Extensions/DialoguesExtension.swift
@@ -10,49 +10,48 @@ import SpriteKit
 import GameplayKit
 
 extension GameScene {
-
+    
     func drawDialogues(won: Bool) {
-
+        
         dialogueIndex = 0
         let animateDuration = 0.3
         let animateVector = 50
         let dialogueAnchor = CGPoint(x: frame.midX - 64, y: frame.midY/3 + 50)
         // adicionando fundo escuro
-        dialogueBackground = DefaultObject(name: "dialogue-background", spriteName: "dialogue-background", size: CGSize(width: frame.width, height: frame.height))
-        if let spriteComponent = dialogueBackground.component(ofType: SpriteComponent.self) {
-            spriteComponent.node.position = CGPoint(x: frame.midX, y: frame.midY)
-            spriteComponent.node.zPosition = ZPositionsCategories.dialogueBackground
-            spriteComponent.node.alpha = 0
-            spriteComponent.node.run(SKAction.fadeAlpha(to: 0.6, duration: animateDuration))
-        }
-        entityManager.add(dialogueBackground)
+        dialogueBackground = SKSpriteNode(imageNamed: "dialogue-background")
+        dialogueBackground.name = "dialogue-background"
+        dialogueBackground.size = CGSize(width: frame.width, height: frame.height)
+        dialogueBackground.position = CGPoint(x: frame.midX, y: frame.midY)
+        dialogueBackground.zPosition = ZPositionsCategories.dialogueBackground
+        dialogueBackground.alpha = 0
+        dialogueBackground.run(SKAction.fadeAlpha(to: 0.6, duration: animateDuration))
+        addChild(dialogueBackground)
         
         // adicionando B.E.E.P.
-        beep = DefaultObject(name: "beep", spriteName: "beep-1", size: CGSize(width: 256, height: 256))
-        if let spriteComponent = beep.component(ofType: SpriteComponent.self) {
-            spriteComponent.node.position = CGPoint(x: frame.midX/3 - CGFloat(animateVector), y: frame.minY + spriteComponent.node.size.height/2)
-            spriteComponent.node.zPosition = ZPositionsCategories.dialogueItems
-            spriteComponent.node.alpha = 0
-            let fadeToLeft = SKAction.group([
-                SKAction.move(by: CGVector(dx: animateVector, dy: 0), duration: animateDuration),
-                SKAction.fadeAlpha(to: 1, duration: 0.5)])
-            spriteComponent.node.run(fadeToLeft)
-        }
-        entityManager.add(beep)
+        beep = SKSpriteNode(imageNamed: "beep-1")
+        beep.name = "beep"
+        beep.size = CGSize(width: 256, height: 256)
+        beep.position = CGPoint(x: frame.midX/3 - CGFloat(animateVector), y: frame.minY + beep.size.height/2)
+        beep.zPosition = ZPositionsCategories.dialogueItems
+        beep.alpha = 0
+        let fadeToLeft = SKAction.group([
+            SKAction.move(by: CGVector(dx: animateVector, dy: 0), duration: animateDuration),
+            SKAction.fadeAlpha(to: 1, duration: 0.5)])
+        beep.run(fadeToLeft)
+        addChild(beep)
         
         // adicionando caixa de diálogo
-        dialogueTab = DefaultObject(name: "beep-dialogue", spriteName: "beep-dialogue")
-        if let spriteComponent = dialogueTab.component(ofType: SpriteComponent.self) {
-            spriteComponent.node.position = CGPoint(x: dialogueAnchor.x + 128 + CGFloat(animateVector), y: dialogueAnchor.y)
-            spriteComponent.node.zPosition = ZPositionsCategories.dialogueTab
-            spriteComponent.node.alpha = 0
-            let fadeToRight = SKAction.group([
-                SKAction.move(by: CGVector(dx: -animateVector, dy: 0), duration: animateDuration),
-                SKAction.fadeAlpha(to: 1, duration: 0.5)])
-            spriteComponent.node.run(fadeToRight)
-        }
-        entityManager.add(dialogueTab)
-
+        dialogueTab = SKSpriteNode(imageNamed: "beep-dialogue")
+        dialogueTab.name = "beep-dialogue"
+        dialogueTab.position = CGPoint(x: dialogueAnchor.x + 128 + CGFloat(animateVector), y: dialogueAnchor.y)
+        dialogueTab.zPosition = ZPositionsCategories.dialogueTab
+        dialogueTab.alpha = 0
+        let fadeToRight = SKAction.group([
+            SKAction.move(by: CGVector(dx: -animateVector, dy: 0), duration: animateDuration),
+            SKAction.fadeAlpha(to: 1, duration: 0.5)])
+        dialogueTab.run(fadeToRight)
+        addChild(dialogueTab)
+        
         if !won {
             dialogueText = SKLabelNode(text: dialogues[dialogueIndex])
             dialogueText.fontSize = 14.0
@@ -94,13 +93,10 @@ extension GameScene {
         dialogueText.horizontalAlignmentMode = .left
         dialogueText.numberOfLines = 2
         dialogueText.alpha = 0
-        let fadeToRight = SKAction.group([
-            SKAction.move(by: CGVector(dx: -animateVector, dy: 0), duration: animateDuration),
-            SKAction.fadeAlpha(to: 1, duration: 0.5)])
         dialogueText.run(fadeToRight)
         addChild(dialogueText)
         
-
+        
         let namePlay: String
         if won {
             namePlay = "next-button"
@@ -108,41 +104,25 @@ extension GameScene {
             namePlay = "play-dialogue"
         }
         // adiciona botão para avançar para o próximo diálogo
-        dialogueButton = DefaultObject(name: namePlay, spriteName: "play-dialogue")
-
-        if let spriteComponent = dialogueButton.component(ofType: SpriteComponent.self) {
-            spriteComponent.node.position = CGPoint(x: dialogueAnchor.x + 380 + CGFloat(animateVector), y: dialogueAnchor.y - 45)
-            spriteComponent.node.zPosition = ZPositionsCategories.dialogueItems
-            spriteComponent.node.alpha = 0
-            let fadeToRight = SKAction.group([
-                SKAction.move(by: CGVector(dx: -animateVector, dy: 0), duration: animateDuration),
-                SKAction.fadeAlpha(to: 1, duration: 0.5)])
-            spriteComponent.node.run(fadeToRight)
-            //            let pulsed = SKAction.sequence([
-            //                SKAction.resize(byWidth: 10, height: 10, duration: 0.5),
-            //                SKAction.resize(byWidth: -10, height: -10, duration: 0.5)])
-            //            spriteComponent.node.run(SKAction.repeatForever(pulsed))
-        }
-        entityManager.add(dialogueButton)
-    
+        dialogueButton = SKSpriteNode(imageNamed: "play-dialogue")
+        dialogueButton.name = namePlay
+        dialogueButton.position = CGPoint(x: dialogueAnchor.x + 380 + CGFloat(animateVector), y: dialogueAnchor.y - 45)
+        dialogueButton.zPosition = ZPositionsCategories.dialogueItems
+        dialogueButton.alpha = 0
+        dialogueButton.run(fadeToRight)
+        addChild(dialogueButton)
+        
         // adiciona botão para avançar para o próximo diálogo
         
         if !won {
-            dialogueSkip = DefaultObject(name: "skip-button")
-            if let spriteComponent = dialogueSkip.component(ofType: SpriteComponent.self) {
-                spriteComponent.node.position = CGPoint(x: dialogueAnchor.x + 378 + CGFloat(animateVector), y: dialogueAnchor.y - 125)
-                spriteComponent.node.zPosition = ZPositionsCategories.dialogueItems
-                spriteComponent.node.alpha = 0
-                let fadeToRight = SKAction.group([
-                    SKAction.move(by: CGVector(dx: -animateVector, dy: 0), duration: animateDuration),
-                    SKAction.fadeAlpha(to: 1, duration: 0.5)])
-                spriteComponent.node.run(fadeToRight)
-            }
-            
-            entityManager.add(dialogueSkip)
+            dialogueSkip = SKSpriteNode(imageNamed: "skip-button")
+            dialogueSkip.name = "skip-button"
+            dialogueSkip.position = CGPoint(x: dialogueAnchor.x + 378 + CGFloat(animateVector), y: dialogueAnchor.y - 125)
+            dialogueSkip.zPosition = ZPositionsCategories.dialogueItems
+            dialogueSkip.alpha = 0
+            dialogueSkip.run(fadeToRight)
+            addChild(dialogueSkip)
         }
-
-
     }
     
     func updateText () {
@@ -150,70 +130,56 @@ extension GameScene {
             dialogueIndex = dialogueIndex + 1
             dialogueText.text = dialogues[dialogueIndex]
         } else {
-
+            
             skipText(next: false)
-
+            
         }
     }
     
     func hintStage () {
-
+        
         drawDialogues(won: false)
     }
     
     func skipText (next: Bool) {
-
-
+        
+        
         let animateDuration = 0.3
         let animateVector = 50
-        if let spriteComponent = dialogueBackground.component(ofType: SpriteComponent.self) {
-            spriteComponent.node.run(SKAction.fadeAlpha(to: 0, duration: animateDuration)) {
-                self.entityManager.remove(self.dialogueBackground)
-            }
+        
+        dialogueBackground.run(SKAction.fadeAlpha(to: 0, duration: animateDuration)) {
+            self.dialogueBackground.removeFromParent()
         }
-        if let spriteComponent = beep.component(ofType: SpriteComponent.self) {
-            let backToLeft = SKAction.group([
-                SKAction.move(by: CGVector(dx: -animateVector, dy: 0), duration: animateDuration),
-                SKAction.fadeAlpha(to: 0, duration: animateDuration)])
-            spriteComponent.node.run(backToLeft) {
-                self.entityManager.remove(self.beep)
-            }
+        
+        let backToLeft = SKAction.group([
+            SKAction.move(by: CGVector(dx: -animateVector, dy: 0), duration: animateDuration),
+            SKAction.fadeAlpha(to: 0, duration: animateDuration)])
+        beep.run(backToLeft) {
+            self.beep.removeFromParent()
         }
-        if let spriteComponent = dialogueButton.component(ofType: SpriteComponent.self) {
-            let backToRight = SKAction.group([
-                SKAction.move(by: CGVector(dx: animateVector, dy: 0), duration: animateDuration),
-                SKAction.fadeAlpha(to: 0, duration: animateDuration)])
-            spriteComponent.node.run(backToRight) {
-                self.entityManager.remove(self.dialogueButton)
-            }
-        }
-        if let spriteComponent = dialogueTab.component(ofType: SpriteComponent.self) {
-            let backToRight = SKAction.group([
-                SKAction.move(by: CGVector(dx: animateVector, dy: 0), duration: animateDuration),
-                SKAction.fadeAlpha(to: 0, duration: animateDuration)])
-            spriteComponent.node.run(backToRight) {
-                self.entityManager.remove(self.dialogueTab)
-            }
-        }
-        if let spriteComponent = dialogueSkip.component(ofType: SpriteComponent.self) {
-            let backToRight = SKAction.group([
-                SKAction.move(by: CGVector(dx: animateVector, dy: 0), duration: animateDuration),
-                SKAction.fadeAlpha(to: 0, duration: animateDuration)])
-            spriteComponent.node.run(backToRight) {
-                self.entityManager.remove(self.dialogueSkip)
-            }
-        }
+        
         let backToRight = SKAction.group([
             SKAction.move(by: CGVector(dx: animateVector, dy: 0), duration: animateDuration),
             SKAction.fadeAlpha(to: 0, duration: animateDuration)])
+        dialogueButton.run(backToRight) {
+            self.dialogueButton.removeFromParent()
+        }
+        
+        dialogueTab.run(backToRight) {
+            self.dialogueTab.removeFromParent()
+        }
+        
+        
+        dialogueSkip.run(backToRight) {
+            self.dialogueSkip.removeFromParent()
+        }
+        
         dialogueText.run(backToRight) {
             self.dialogueText.removeFromParent()
-
+            
             if next {
                 self.returnToMap()
             }
-
-
         }
     }
 }
