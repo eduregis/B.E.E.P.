e8864e6c26e254ab41c31fdfdd639419e3837bd6
Eduardo Regis <eduregis1@gmail.com>
2020-06-24 10:22:14 -0300

    dialogos iniciais na tela de mapa

--- B.E.E.P./Scenes/Extensions/DialoguesMapSceneExtension.swift
+++ B.E.E.P./Scenes/Extensions/DialoguesMapSceneExtension.swift
@@ -177,5 +177,8 @@ extension MapScene {
         dialogueText.run(backToRight) {
             self.dialogueText.removeFromParent()
         }
+        let dialoguesInitial = BaseOfDialogues.buscar(id: "menu-stage-\(lastStageAvailable)")
+        guard let dialogues = dialoguesInitial else { return }
+        self.dialogues = dialogues.text
     }
 }
